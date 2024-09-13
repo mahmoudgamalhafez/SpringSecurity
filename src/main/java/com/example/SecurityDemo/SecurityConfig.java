5939d1b0b276678e7b10e6bd923a4bbeccf473da
mahmoud gamal <mahmoudgamalhafez@gmail.com>
2024-09-14 00:43:13 +0300

    inMemoryAuthenticationEX

--- src/main/java/com/example/SecurityDemo/SecurityConfig.java
+++ src/main/java/com/example/SecurityDemo/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,5 +23,15 @@ public class SecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+    @Bean
+    public UserDetailsService  userDetailsService(){
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}password1")
+                .roles("USER").build();
+        UserDetails user2 = User.withUsername("user2")
+                .password("{noop}password1")
+                .roles("USER").build();
+        return new InMemoryUserDetailsManager(user1,user2);
     }
 }
