a36f846f42c76f87fbfd5f61320c5a09e699afb0
mahmoud gamal <mahmoudgamalhafez@gmail.com>
2024-09-13 22:51:27 +0300

    init spring security

---
+++ src/main/java/com/example/SecurityDemo/GreetingsController.java
@@ -0,0 +1,13 @@
+package com.example.SecurityDemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+    @GetMapping("/hello")
+    public String sayhello(){
+        return "hello";
+    }
+}
